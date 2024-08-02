31021b4ecdf8a1ab1964fdbae11eb47e1ea83dd2
wryte123 <oepkf@foxmail.com>
2024-08-02 16:33:26 +0800

    chore: Update UiButton component to include search icon

--- packages/components/src/ui-button/UiButton.vue
+++ packages/components/src/ui-button/UiButton.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { Button } from "ant-design-vue";
+import { SearchOutlined } from "@ant-design/icons-vue";
+import "ant-design-vue/dist/antd.css";
 </script>
 
 <template>
-	<Button danger>
-		<slot></slot>
-	</Button>
+  <Button danger>
+    <template #icon><SearchOutlined /></template>
+    <slot></slot>
+  </Button>
 </template>
 
 <style lang="less"></style>
+
